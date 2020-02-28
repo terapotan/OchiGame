8f4bd7367619525c89c1ecfb36c4d1f90c257a4a
terapotan <rs582855@pc4.so-net.ne.jp>
2020-02-28 20:09:41 +0900

    繝峨Ο繧､繝牙菅繧貞虚縺九○繧九ｈ縺�縺ｫ謖吝虚螟画峩

---
+++ OchiGame/Assets/Scenes/playScene/AndroidMotionControler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidMotionControler : MonoBehaviour
+{
+    private Rigidbody2D Rigid2D;
+
+    private const float ANDROIDKUN_FORCE_SIZE = 50.0f;
+    private const float MAX_SPEED_WALKING_ANDROIDKUN = 3.0f;
+    private const int RIGHT = 1;
+    private const int LEFT = -1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int AndroidKunDirection = 0;
+
+
+        if (Input.GetKey(KeyCode.RightArrow)) AndroidKunDirection = RIGHT;
+        if (Input.GetKey(KeyCode.LeftArrow)) AndroidKunDirection = LEFT;
+
+        float nowSpeedWalkingAndroidkun = Mathf.Abs(Rigid2D.velocity.x);
+
+        if(nowSpeedWalkingAndroidkun < MAX_SPEED_WALKING_ANDROIDKUN)
+        {
+            Rigid2D.AddForce(transform.right * AndroidKunDirection * ANDROIDKUN_FORCE_SIZE);
+        }
+    }
+}
