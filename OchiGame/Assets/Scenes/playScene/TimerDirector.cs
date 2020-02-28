19d5465ae59caf0a2f8e12f22efba043cc9d3fb3
terapotan <rs582855@pc4.so-net.ne.jp>
2020-02-28 22:51:15 +0900

    繧ｿ繧､繝槭�ｼ讖溯�ｽ繧貞ｮ溯｣�

---
+++ OchiGame/Assets/Scenes/playScene/TimerDirector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TimerDirector : MonoBehaviour
+{
+    GameObject TimerValueText;
+    private float ElapsedTime = 0.0f;
+    private int ElapsedTimeInStage = 100;//単位は秒
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimerValueText = GameObject.Find("TimerValueText");
+        TimerValueText.GetComponent<Text>().text = ElapsedTimeInStage.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ElapsedTime += Time.deltaTime;
+
+        if (ElapsedTime > 1.0f)
+        {
+            ElapsedTime = 0;
+            ElapsedTimeInStage--;
+            TimerValueText.GetComponent<Text>().text = ElapsedTimeInStage.ToString();
+        }
+    }
+}
