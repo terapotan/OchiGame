3607c8edf8e135a4238f5d630653e80a0be3dc9e
terapotan <rs582855@pc4.so-net.ne.jp>
2020-02-28 21:58:59 +0900

    ExtendScreen縺ｮ繝�繧ｹ繝医ｒ霑ｽ蜉

--- OchiGame/Assets/Scenes/playScene/ArrowController.cs
+++ OchiGame/Assets/Scenes/playScene/ArrowController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ArrowController : MonoBehaviour
 {
+    public GameObject ArrowPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
