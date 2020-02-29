a7bea3ac282ae981efa98e035fd7cb9117f10286
terapotan <rs582855@pc4.so-net.ne.jp>
2020-02-29 17:34:50 +0900

    繝�繧ｹ繝郁ｿｽ蜉

--- manualTest/testMain.py
+++ manualTest/testMain.py
@@ -19,6 +19,10 @@ class testMain(unittest.TestCase):
         self.assertEqual(input('タイトル画面からステージ選択画面へ移動出来るか(y/n)'),'y')
     def test_canMoveGameTitleScene(self):
         self.assertEqual(input('ステージ選択画面からタイトル画面に移動できるか(y/n)'),'y')
+    def test_canMoveStageSelectSceneInGameOver(self):
+        self.assertEqual(input('ゲームオーバー画面からステージ選択画面に戻れるか(y/n)'),'y')
+    def test_canMoveStageSelectSceneInGameClear(self):
+        self.assertEqual(input('ステージクリア画面からステージ選択画面に戻れるか(y/n)'),'y')
 
 if __name__ == '__main__':
     unittest.main(exit=False)
