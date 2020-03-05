a490658c6c20e7c29098e0a5d0d4d81f1d1d15d1
terapotan <rs582855@pc4.so-net.ne.jp>
2020-03-05 17:08:18 +0900

    test

--- OchiGame/Assets/Scenes/playScene/FallEventGenerator.cs
+++ OchiGame/Assets/Scenes/playScene/FallEventGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowGenerator : MonoBehaviour
+public class FallEventGenerator : MonoBehaviour
 {
 
     public GameObject ArrowPrefab;
