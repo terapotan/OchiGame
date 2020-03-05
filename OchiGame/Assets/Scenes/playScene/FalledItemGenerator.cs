b4f1decbb88ba59e0d52b18e4a03e56760f6a812
terapotan <rs582855@pc4.so-net.ne.jp>
2020-03-05 19:17:24 +0900

    Add probability calculation function

--- OchiGame/Assets/Scenes/playScene/FalledItemGenerator.cs
+++ OchiGame/Assets/Scenes/playScene/FalledItemGenerator.cs
@@ -11,6 +11,14 @@ public class FalledItemGenerator : MonoBehaviour
 
     private GameObject[] FallingItems;
 
+    private int[] ProbabilityList = 
+    {
+        25,
+        25,
+        25,
+        25
+    };
+
     public void Start()
     {
         FallingItems = new GameObject[]
@@ -24,11 +32,40 @@ public class FalledItemGenerator : MonoBehaviour
 
     public void GenerateFalledItem()
     {
-        int GeneratingIndex = Random.Range(0, 4);
+        int GeneratingIndex = CalculateProbabilistic(ProbabilityList);
+
         GameObject tmpGameObject = Instantiate(FallingItems[GeneratingIndex]) as GameObject;
 
        
         int xGeneratedArrow = Random.Range(-6, 7);
         tmpGameObject.transform.position = new Vector3(xGeneratedArrow, 5.5f, 0);
+    }
+
+    /// <summary>
+    /// <para>引数で渡された確率のリスト(百分率)に従って数値を返す。 </para>
+    /// <para>リストに10,80,10と指定されたら10％の確率で0を,80％で1,10％で2を返す。</para>
+    /// <para>リストの数値の合計値は100である必要がある。</para>
+    /// </summary>
+    /// <param name="probabilityList"></param>
+    /// <returns></returns>
+    private int CalculateProbabilistic(int[] ProbabilityList)
+    {
+        int ReturnedIndex = 0;
+        int RandomValue = Random.Range(1, 101);//101は含まない
+        int MaxValue = 0;
+        int MinValue = 1;
+
+        foreach (int i in ProbabilityList)
+        {
+            MaxValue += i;
+            if(MinValue <= RandomValue && RandomValue <= MaxValue)
+            {
+                break;
+            }
+            MinValue += i;
+            ReturnedIndex++;
+        }
+
+        return ReturnedIndex;
     }
 }
