a47b2521f932dc743dc80d07bdeb4d287223f17f
terapotan <rs582855@pc4.so-net.ne.jp>
2020-03-05 18:32:45 +0900

    Change test

--- manualTest/itemActionTest.py
+++ manualTest/itemActionTest.py
@@ -7,6 +7,8 @@ class itemActionTest(unittest.TestCase):
         self.assertEqual(input('スピードアップのアイテムが落ちてくるか(y/n)'),'y')
     def test_fallSpeedDownItem(self):
         self.assertEqual(input('スピードダウンのアイテムが落ちてくるか(y/n)'),'y')
+    def test_fallArrow(self):
+        self.assertEqual(input('矢は落ちてくるか(y/n)'),'y')
 
 if __name__ == '__main__':
     unittest.main(exit=False)
