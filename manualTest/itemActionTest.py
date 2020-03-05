4fb83332f6f2f662fc26efea236f6caeb961e4c4
terapotan <rs582855@pc4.so-net.ne.jp>
2020-03-05 16:50:36 +0900

    Add test

---
+++ manualTest/itemActionTest.py
@@ -0,0 +1,13 @@
+import unittest
+
+class itemActionTest(unittest.TestCase):
+    def test_fallStartItem(self):
+        self.assertEqual(input('スターのアイテムが落ちてくるか(y/n)'),'y')
+    def test_fallSpeedUpItem(self):
+        self.assertEqual(input('スピードアップのアイテムが落ちてくるか(y/n)'),'y')
+    def test_fallSpeedDownItem(self):
+        self.assertEqual(input('スピードダウンのアイテムが落ちてくるか(y/n)'),'y')
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+    input()
