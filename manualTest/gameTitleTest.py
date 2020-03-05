7f32c81a05fde0c1c56f66ad0723e05037562401
terapotan <rs582855@pc4.so-net.ne.jp>
2020-03-05 13:18:05 +0900

    Change file structure of manual Test

--- manualTest/gameTitleTest.py
+++ manualTest/gameTitleTest.py
@@ -1,7 +1,6 @@
 import unittest
 
-class testMain(unittest.TestCase):
-    """
+class gameTitleTest(unittest.TestCase):
     def test_canMoveAndroid(self):
         self.assertEqual(input('アンドロイド君を→と←に動かせるか(y/n)'),'y')
     def test_fallManyArrows(self):
@@ -14,18 +13,7 @@ class testMain(unittest.TestCase):
         self.assertEqual(input('タイマーの数値が0になったらクリア画面に移動するか(y/n)'),'y')
     def test_isGameOvered(self):
         self.assertEqual(input('ドロイド君が矢に当たったらゲームオーバー画面に飛ぶか(y/n)'),'y')
-    """
-    def test_canMoveStageSelectScene(self):
-        self.assertEqual(input('タイトル画面からステージ選択画面へ移動出来るか(y/n)'),'y')
-    def test_canMoveGameTitleScene(self):
-        self.assertEqual(input('ステージ選択画面からタイトル画面に移動できるか(y/n)'),'y')
-    def test_canMoveStageSelectSceneInGameOver(self):
-        self.assertEqual(input('ゲームオーバー画面からステージ選択画面に戻れるか(y/n)'),'y')
-    def test_canMoveStageSelectSceneInGameClear(self):
-        self.assertEqual(input('ステージクリア画面からステージ選択画面に戻れるか(y/n)'),'y')
- 
+
 if __name__ == '__main__':
     unittest.main(exit=False)
     input()
-    
-    
